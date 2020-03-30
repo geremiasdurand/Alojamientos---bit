e324d4c33d433c332b2dcac051a2585a37d44d68
geremiasdurand <30659129+geremiasdurand@users.noreply.github.com>
2020-03-30 15:19:11 -0300

    Contacto y about us
    Se añadieron las paginas de contacto junto con las de quienes somos.
    
    Hay que modificar su html

--- inicio/urls.py
+++ inicio/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'inicio'
 urlpatterns = [
     path("", views.index),
+    path("aboutus", views.aboutus),
+    path("contacto", views.contacto),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'inicio/index.html')
+
+def aboutus(request):
+    return render(request, "inicio/aboutus.html")
+
+def contacto(request):
+    return render(request, "inicio/contacto.html")
